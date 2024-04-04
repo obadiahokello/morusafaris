9834f5dbec56251c8853d620f917f68f56a6a202
obadiahokello <145970565+obadiahokello@users.noreply.github.com>
2024-04-04 09:39:07 +0300

    addded requirement and static dirs

--- Morusafaris/settings.py
+++ Morusafaris/settings.py
@@ -119,10 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_URL = 'images/'
